edc52e1559accf25df0292f24072a4179020b858
xReggimon <r-mcmullen.1@onu.edu>
2024-04-25 08:13:28 -0400

    Completed P11-1

--- Source.cpp
+++ Source.cpp
@@ -1,109 +1,110 @@
 #include <iostream>
-
 using namespace std;
 
-#define ROWS 13
-#define COLS 13
-#define ERROR -1
+const int ROWS = 13;
+const int COLS = 8;
 
 int findRowIndex(int thisRow);
 int findSeatIndex(char thisSeat);
 void displaySeatColumn(char seats[][ROWS], int column);
 
-/*char seatingChart[COLS][ROWS] = {
-	{'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D'},
-	{'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C'},
-	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1', '1', '1', '1'},
-	{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3'},
-	{'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'},
-	{'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A'}
-};*/
+int row;
+char cols;
+int arrayRow;
+int arrayCol;
 
-char seatingChart[COLS][ROWS] = {
-	{'D', 'C', ' ', '1', 'B', 'A'},
-	{'D', 'C', ' ', '2', 'B', 'A'},
-	{'D', 'C', ' ', '3', 'B', 'A'},
-	{'D', 'C', ' ', '4', 'B', 'A'},
-	{'D', 'C', ' ', '5', 'B', 'A'},
-	{'D', 'C', ' ', '6', 'B', 'A'},
-	{'D', 'C', ' ', '7', 'B', 'A'},
-	{'D', 'C', ' ', '8', 'B', 'A'},
-	{'D', 'C', ' ', '9', 'B', 'A'},
-	{'D', 'C', '1', '0', 'B', 'A'},
-	{'D', 'C', '1', '1', 'B', 'A'},
-	{'D', 'C', '1', '2', 'B', 'A'},
-	{'D', 'C', '1', '3', 'B', 'A'},
+char seats[COLS][ROWS] = {
+	{'D','D','D','D','D','D','D','D','D','D','D','D',' '},
+	{'C','C','C','C','C','C','C','C','C','C','C','C',' '},
+	{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+	{' ',' ',' ',' ',' ',' ',' ',' ',' ','1','1','1','1'},
+	{'1','2','3','4','5','6','7','8','9','0','1','2','3'},
+	{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+	{'B','B','B','B','B','B','B','B','B','B','B','B','B'},
+	{'A','A','A','A','A','A','A','A','A','A','A','A','A'}
 };
-
 
 
 int main() {
 	
-	string seat;
-	int row, col;
-	char seatLetter;
-	int seatNumber;
+	for (int i = 0; i < COLS; i++) {
+		displaySeatColumn(seats, i);
+		cout << endl;
+	}
 
-	do {
-		//Displays seating Chart
-		for (int i = 0; i < ROWS; i++) {
-			displaySeatColumn(seatingChart, i);
+	cout << "---------------------------------" << endl;
+
+	cout << "Enter a seat or Q to quit: ";
+
+	while (cin >> row >> cols) {
+		arrayCol = findSeatIndex(cols);
+		arrayRow = findRowIndex(row);
+		if (cols == 'D' || cols == 'C') {
+			if (row == 13) {
+				cout << "Sorry, no such seat exists on the CRJ 200." << endl;
+				cout << "Enter a seat or Q to quit: ";
+				continue;
+			}
 		}
-		cout << "Enter a seat or Q to quit: ";
-		cin >> seat;
-
-		if (seat[0] == 'Q' || seat[0] == 'q')
-			break;
-
-		seatLetter = seat[1];
-		seatNumber = seat[0] - '0';
-
-		row = findRowIndex(seatNumber);
-		col = findSeatIndex(seatLetter);
-
-		//Occupy seats or indicate error
-		if (row == ERROR || col == ERROR) {
-			cout << "Sorry, no such seat exists on the CRJ 200. " << endl;
+		if (arrayRow == -1) {
+			cout << "Sorry, no such seat exists on the CRJ 200." << endl;
+			cout << "Enter a seat or Q to quit: ";
+			continue;
+		}
+		if (arrayCol == -1) {
+			cout << "Sorry, no such seat exists on the CRJ 200." << endl;
+			cout << "Enter a seat or Q to quit: ";
+			continue;
+		}
+		if (seats[arrayCol][arrayRow] != 'X') {
+			seats[arrayCol][arrayRow] = 'X';
 		}
 		else {
-			if (seatingChart[col][row] != 'X') {
-				seatingChart[col][row] = 'X';
-				cout << "Seat " << seat << " reserved successfully." << endl;
-			}
-			else {
-				cout << "Seat " << seat << " is already taken." << endl;
-			}
+			cout << "This seat is already taken, please pick another seat." << endl;
+			cout << "Enter a seat or Q to quit: ";
+			continue;
 		}
-	} while (true);
-
+		
+		for (int i = 0; i < COLS; i++) {
+			displaySeatColumn(seats, i);
+			cout << endl;
+		}
+		cout << "---------------------------" << endl;
+		cout << "Enter a seat or Q to quit: ";
+	}
 	return 0;
-
 }
 
+//finds the desired row
 int findRowIndex(int thisRow) {
-	if (thisRow >= 1 && thisRow <= ROWS) {
+	if (thisRow < 14 && thisRow > 0) {
 		return thisRow - 1;
 	}
 	else {
-		return ERROR;
+		return -1;
 	}
 }
 
+//finds the desired column
 int findSeatIndex(char thisSeat) {
-	for (int i = 0; i < COLS; i++) {
-			if (seatingChart[i][0] == thisSeat) {
-				return i;
-			}
-		}
-	return ERROR;
+	switch (thisSeat) {
+	case 'A':
+		return 7;
+	case 'B':
+		return 6;
+	case 'C':
+		return 1;
+	case 'D':
+		return 0;
+	default:
+		return -1;
+	}
 }
-
+//updates the seating chart
 void displaySeatColumn(char seats[][ROWS], int column) {
 
 	for (int i = 0; i < ROWS; i++) {
-			cout << seats[i][column] << " ";
+			cout << seats[column][i] << " ";
 	}
-	cout << endl;
-	
 }
 
